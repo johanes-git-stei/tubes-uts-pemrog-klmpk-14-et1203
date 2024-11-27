95a53c7d1bbf3e40019f5d6b2750f37dab2177c7
dunzuaa <mahardwitiyajatip@gmail.com>
2024-11-27 20:41:02 +0700

    main page done, perlu ditambahin preview thumbnail video-video yang udah di upload

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def allowed_file(filename):
 def home():
     if "user" in session:
         return redirect(url_for("browse_videos"))
-    return render_template("login.html")
+    return render_template("index.html")
 
 # Route: Login
 @app.route("/login", methods=["GET", "POST"])
